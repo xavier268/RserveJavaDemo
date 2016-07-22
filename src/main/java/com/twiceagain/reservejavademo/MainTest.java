cc0b3533e08e6213f42a66b6efa0f887a6b53b30
xavier <xavier@gandillot.com>
2016-07-22 13:13:17 +0200

    refactoring

--- src/main/java/com/twiceagain/reservejavademo/MainTest.java
+++ src/main/java/com/twiceagain/reservejavademo/MainTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.twiceagain.testrserve;
+package com.twiceagain.reservejavademo;
 
 import java.util.Arrays;
 import java.util.logging.Level;
